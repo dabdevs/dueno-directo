bd02ede5b0e76150bfa7afff033651afa47ef296
dabdevs <dabdevs@gmail.com>
2023-07-30 21:31:17 -0300

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,73 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required />
+            </div>
+
+            <!-- Account type -->
+            <div class="mt-4">
+                <x-label for="type" :value="__('I am:')"/>
+
+                <div class="flex mt-2">
+                    <x-input id="owner" class="mx-1"
+                                    type="radio"
+                                    name="type" 
+                                    value="owner" required />
+                    <x-label for="owner" :value="__('owner')" />
+                    
+                    <x-input id="tenant" class="mx-1"
+                                    type="radio"
+                                    value="tenant"
+                                    name="type" required />
+                    <x-label for="tenant" :value="__('tenant')" />
+                    
+                </div>
+                
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-button class="ml-4">
+                    {{ __('Register') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
